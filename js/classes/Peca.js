f766a9a0b33035da9df69eb919eeb5713f0f58b3
Vivi <vivifeitos@gmail.com>
2025-09-19 17:01:43 -0300

    oiii pedro olha a pepeca

---
+++ js/classes/Peca.js
@@ -0,0 +1,21 @@
+export class Peca {
+    constructor(cor) {
+        if (this.constructor === Peca) {
+            throw new Error("A classe 'Peca' é abstrata e não pode ser instanciada diretamente.");
+        }
+        this.cor = cor;
+    }
+
+    get tipo() {
+        return this.constructor.name.toLowerCase();
+    }
+
+    get valor() {
+        const PONTOS = { 'peao': 1, 'cavalo': 3, 'bispo': 3, 'torre': 5, 'dama': 9, 'rei': 0 };
+        return PONTOS[this.tipo] || 0;
+    }
+
+    getMovimentosValidos(posicaoAtual, tabuleiro) {
+        throw new Error(`O método 'getMovimentosValidos' não foi implementado para a classe '${this.constructor.name}'.`);
+    }
+}
