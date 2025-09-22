dc76a8ac6c8e9a1656b53ebbba58bc90260b4514
Monique <moniquemenezes@hotmail.com>
2025-09-22 18:32:21 -0300

    notacao do histórico(teste)

--- js/classes/Tabuleiro.js
+++ js/classes/Tabuleiro.js
@@ -64,6 +64,12 @@ export class Tabuleiro {
         this.historico.push(`${this.posicaoParaNotacao(posInicial)} -> ${this.posicaoParaNotacao(posFinal)}`);
 
     }
+    
+    getUltimoMovimento() {
+    return this.historico[this.historico.length - 1];
+    } // pedro ve se esse método serve
+
+
 
     posicaoParaNotacao(pos) {
         const colunas = ['a','b','c','d','e','f','g','h'];
